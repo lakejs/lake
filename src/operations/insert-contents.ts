a677255df338b6faaee78abc007f89a3c59d169a
luolonghao <luolonghao@gmail.com>
2023-10-16 13:42:09 +0800

    chore(insertContents): remove return value

--- src/operations/insert-contents.ts
+++ src/operations/insert-contents.ts
@@ -1,14 +1,13 @@
 import { query } from '../utils';
-import { Nodes } from '../models/nodes';
 import { Range } from '../models/range';
 import { deleteContents } from './delete-contents';
 import { insertFragment } from './insert-fragment';
 
 // Inserts a HTML string into the specified range.
-export function insertContents(range: Range, value: string): Nodes {
+export function insertContents(range: Range, value: string): void {
   const nodes = query(value);
   if (!range.commonAncestor.isContentEditable) {
-    return nodes;
+    return;
   }
   if (!range.isCollapsed) {
     deleteContents(range);
@@ -18,5 +17,4 @@ export function insertContents(range: Range, value: string): Nodes {
     fragment.appendChild(nativeNode);
   });
   insertFragment(range, fragment);
-  return nodes;
 }
