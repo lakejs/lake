113b4cf371d981f72bca1f765e633b858e812f3d
luolonghao <luolonghao@gmail.com>
2023-11-04 18:20:46 +0800

    feat(backspace): implement plugin

--- src/plugins/backspace.ts
+++ src/plugins/backspace.ts
@@ -1,46 +1,43 @@
 import type Editor from '..';
+import { mergeNodes } from '../utils';
+import { Range } from '../models';
 import { adjustStartAttributes } from './list';
-
-// selection.getLeftText()
-
-function setParagraph(editor: Editor) {
-  editor.selection.setBlocks('<p />');
-  adjustStartAttributes(editor);
-  editor.history.save();
-  editor.select();
-}
+import { setBlocks } from '../operations/set-blocks';
 
 export default (editor: Editor) => {
   editor.keystroke.setKeydown('backspace', event => {
-    event.preventDefault();
     const selection = editor.selection;
-    let block = selection.getBlocks()[0];
-    if (!block) {
-      setParagraph(editor);
-      return;
-    }
-    if (block.isEmpty && block.name !== 'p') {
-      setParagraph(editor);
-      return;
-    }
-    const rightText = selection.getRightText();
-    selection.splitBlock();
-    if (rightText !== '') {
+    const range = selection.range;
+    if (!range.isCollapsed) {
+      selection.deleteContents();
       adjustStartAttributes(editor);
       editor.history.save();
       editor.select();
       return;
     }
-    block = selection.getBlocks()[0];
-    if (block.isHeading) {
-      setParagraph(editor);
-      return;
+    const leftText = selection.getLeftText();
+    if (leftText === '') {
+      event.preventDefault();
+      const block = selection.getBlocks()[0];
+      if (!block) {
+        editor.selection.setBlocks('<p />');
+      }
+      let prevBlock = block.prev();
+      if (prevBlock.length === 0) {
+        return;
+      }
+      if (!prevBlock.isBlock) {
+        const prevRange = new Range();
+        prevRange.selectNodeContents(prevBlock);
+        setBlocks(prevRange, '<p />');
+        prevBlock = prevBlock.closestBlock();
+      }
+      const bookmark = selection.insertBookmark();
+      mergeNodes(prevBlock, block);
+      selection.toBookmark(bookmark);
+      adjustStartAttributes(editor);
+      editor.history.save();
+      editor.select();
     }
-    if (block.isList && block.attr('type') === 'checklist') {
-      block.find('li').attr('value', 'false');
-    }
-    adjustStartAttributes(editor);
-    editor.history.save();
-    editor.select();
   });
 };
