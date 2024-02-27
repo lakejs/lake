ab41398c3dcbb5d39bb1ac3a5b2234b553d69c88
luolonghao <luolonghao@gmail.com>
2024-02-27 20:12:48 +0800

    chore(example): render specified page by current url

--- examples/index.ts
+++ examples/index.ts
@@ -12,35 +12,113 @@ declare global {
 
 window.DEBUG = true;
 
-const menuNode = query('.menu');
-let timeoutId: number | null = null;
-menuNode.on('mouseenter', () => {
-  if (timeoutId) {
-    window.clearTimeout(timeoutId);
-    timeoutId = null;
+type MenuItem = {
+  key: string,
+  url: string,
+  text: string,
+};
+
+const menuItems: MenuItem[] = [
+  {
+    key: 'default',
+    url: './',
+    text: 'Default configuration',
+  },
+  {
+    key: 'document-editor',
+    url: './document-editor',
+    text: 'Document editor',
+  },
+  {
+    key: 'miniature-toolbar',
+    url: './miniature-toolbar',
+    text: 'Miniature toolbar',
+  },
+  {
+    key: 'headless',
+    url: './headless-editor',
+    text: 'Headless editor',
+  },
+  {
+    key: 'mobile',
+    url: './mobile-editor',
+    text: 'Mobile friendly editor',
+  },
+  {
+    key: 'i18n',
+    url: './i18n',
+    text: 'Internationalization',
+  },
+  {
+    key: 'huge-document',
+    url: './huge-document',
+    text: 'Huge Document',
+  },
+];
+
+function renderMenu(): void {
+  const menuNode = query('.menu');
+  const ul = menuNode.find('ul');
+  for (const item of menuItems) {
+    ul.append(`<li><a href="${item.url}">${item.text}</a></li>`);
   }
-  menuNode.find('button').addClass('hovered');
-  menuNode.find('ul').show();
-});
-menuNode.on('mouseleave', () => {
-  timeoutId = window.setTimeout(() => {
-    menuNode.find('button').removeClass('hovered');
-    menuNode.find('ul').hide();
-  }, 300);
-});
+  let timeoutId: number | null = null;
+  menuNode.on('mouseenter', () => {
+    if (timeoutId) {
+      window.clearTimeout(timeoutId);
+      timeoutId = null;
+    }
+    menuNode.find('button').addClass('hovered');
+    menuNode.find('ul').show();
+  });
+  menuNode.on('mouseleave', () => {
+    timeoutId = window.setTimeout(() => {
+      menuNode.find('button').removeClass('hovered');
+      menuNode.find('ul').hide();
+    }, 300);
+  });
+}
 
-const localStorageKey = 'lake-example';
-const editorValue = localStorage.getItem(localStorageKey) ?? defaultValue;
+function renderTitle(key: string): void {
+  const menuItemMap: Map<string, MenuItem> = new Map();
+  for (const item of menuItems) {
+    menuItemMap.set(item.key, item);
+  }
+  const currentItem = menuItemMap.get(key);
+  if (!currentItem) {
+    return;
+  }
+  const titleNode = query('.header .title');
+  titleNode.html(currentItem.text);
+}
 
-const editor = new Editor('.lake-container', {
-  readonly: false,
-  defaultValue: editorValue,
-});
-editor.event.on('change', value => {
-  localStorage.setItem(localStorageKey, value);
-});
-editor.render();
+function renderEditor(key: string): void {
+  const localStorageKey = `lake-example-${key}`;
+  const editorValue = localStorage.getItem(localStorageKey) ?? defaultValue;
+  const editor = new Editor('.lake-container', {
+    readonly: false,
+    defaultValue: editorValue,
+  });
+  editor.event.on('change', value => {
+    localStorage.setItem(localStorageKey, value);
+  });
+  editor.render();
+  new Toolbar(editor).render('.lake-toolbar');
+  window.editor = editor;
+}
 
-new Toolbar(editor).render('.lake-toolbar');
+function renderPage(): void {
+  const url = window.location.href;
+  let key = menuItems[0].key;
+  for (const item of menuItems) {
+    if (url.indexOf(item.key) >= 0) {
+      key = item.key;
+      break;
+    }
+  }
+  renderTitle(key);
+  renderMenu();
+  renderEditor(key);
+}
 
-window.editor = editor;
+renderPage();
