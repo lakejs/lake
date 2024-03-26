d3723fcdf0bb513b0c9f61b77d52e393e4ab563d
luolonghao <luolonghao@gmail.com>
2024-03-26 20:41:39 +0800

    chore: simplify global declaration for expect

--- tests/global.d.ts
+++ tests/global.d.ts
@@ -1,6 +1,7 @@
-declare global {
-  // eslint-disable-next-line vars-on-top, no-var
-  var expect: typeof chai.expect;
-}
+// If your file hasn't any import or export line, this file would be executed
+// in global scope that all declaration in it are visible outside this file.
+// All magic come from var. Replace var with let or const won't work.
+// See more in https://stackoverflow.com/questions/38906359/create-a-global-variable-in-typescript
 
-export {};
+// eslint-disable-next-line no-var
+declare var expect: typeof chai.expect;
