3014f538564bfbc561f885118c7aed13f831f194
luolonghao <luolonghao@gmail.com>
2024-04-10 22:13:30 +0800

    feat(code block): add SQL language; use custom setting instead of basicSetup; customize highlight style

--- src/boxes/code-block.ts
+++ src/boxes/code-block.ts
@@ -1,10 +1,15 @@
 import {
-  basicSetup,
   EditorState,
   Compartment,
   EditorView,
   keymap,
+  history,
+  defaultKeymap,
+  historyKeymap,
   indentWithTab,
+  HighlightStyle,
+  syntaxHighlighting,
+  tags,
   langItems,
 } from '../codemirror';
 import { BoxComponent } from '../types/box';
@@ -14,6 +19,50 @@ import { Dropdown } from '../ui/dropdown';
 import { locale } from '../i18n';
 
 const defaultLangValue = 'text';
+
+const config = {
+  dark: false,
+  background: '#0000000a',
+  foreground: '#444d56',
+  selection: '#1ba2e333',
+  cursor: '#044289',
+  keyword: '#cf222e',
+  variable: '#1f2328',
+  parameter: '#24292e',
+  function: '#005cc5',
+  string: '#0a3069',
+  constant: '#0550ae',
+  type: '#24292f',
+  class: '#24292e',
+  number: '#0550ae',
+  comment: '#57606a',
+  heading: '#0550ae',
+  invalid: '#f6f8fa',
+  regexp: '#116329',
+};
+
+const lightHighlightStyle = HighlightStyle.define([
+  { tag: tags.keyword, color: config.keyword },
+  { tag: [tags.name, tags.deleted, tags.character, tags.macroName], color: config.variable },
+  { tag: [tags.propertyName], color: config.function },
+  { tag: [tags.processingInstruction, tags.string, tags.inserted, tags.special(tags.string)], color: config.string },
+  { tag: [tags.function(tags.variableName), tags.labelName], color: config.function },
+  { tag: [tags.color, tags.constant(tags.name), tags.standard(tags.name)], color: config.constant },
+  { tag: [tags.definition(tags.name), tags.separator], color: config.variable },
+  { tag: [tags.className], color: config.class },
+  { tag: [tags.number, tags.changed, tags.annotation, tags.modifier, tags.self, tags.namespace], color: config.number },
+  { tag: [tags.typeName], color: config.type },
+  { tag: [tags.operator, tags.operatorKeyword], color: config.keyword },
+  { tag: [tags.url, tags.escape, tags.regexp, tags.link], color: config.regexp },
+  { tag: [tags.meta, tags.comment], color: config.comment },
+  { tag: tags.strong, fontWeight: 'bold' },
+  { tag: tags.emphasis, fontStyle: 'italic' },
+  { tag: tags.link, textDecoration: 'underline' },
+  { tag: tags.heading, fontWeight: 'bold', color: config.heading },
+  { tag: [tags.atom, tags.bool, tags.special(tags.variableName)], color: config.variable },
+  { tag: tags.invalid, color: config.invalid },
+  { tag: tags.strikethrough, textDecoration: 'line-through' },
+]);
 
 const langItemMap: Map<string, typeof langItems[0]> = new Map();
 for (const item of langItems) {
@@ -59,9 +108,14 @@ export const codeBlockBox: BoxComponent = {
       parent: codeBlockNativeNode,
       doc: boxValue.code ?? '',
       extensions: [
-        basicSetup,
         EditorState.readOnly.of(editor.readonly),
-        keymap.of([indentWithTab]),
+        history(),
+        keymap.of([
+          ...defaultKeymap,
+          ...historyKeymap,
+          indentWithTab,
+        ]),
+        syntaxHighlighting(lightHighlightStyle),
         language.of(langItem && langItem.component ? langItem.component() : []),
         updateListener,
       ],
--- src/codemirror.ts
+++ src/codemirror.ts
@@ -1,8 +1,7 @@
-import { basicSetup } from 'codemirror';
 import { EditorState, Compartment } from '@codemirror/state';
 import { EditorView, keymap } from '@codemirror/view';
-import { indentWithTab } from '@codemirror/commands';
-import type { LanguageSupport } from '@codemirror/language';
+import { history, defaultKeymap, historyKeymap, indentWithTab } from '@codemirror/commands';
+import { HighlightStyle, syntaxHighlighting, LanguageSupport } from '@codemirror/language';
 import { cpp } from '@codemirror/lang-cpp';
 import { css } from '@codemirror/lang-css';
 import { go } from '@codemirror/lang-go';
@@ -14,8 +13,10 @@ import { markdown } from '@codemirror/lang-markdown';
 import { php } from '@codemirror/lang-php';
 import { python } from '@codemirror/lang-python';
 import { rust } from '@codemirror/lang-rust';
+import { sql } from '@codemirror/lang-sql';
 import { xml } from '@codemirror/lang-xml';
 import { yaml } from '@codemirror/lang-yaml';
+import { tags } from '@lezer/highlight';
 
 type LangItem = {
   value: string,
@@ -37,18 +38,24 @@ const langItems: LangItem[] = [
   { value: 'markdown', text: 'Markdown', component: markdown },
   { value: 'php', text: 'PHP', component: php },
   { value: 'python', text: 'Python', component: python },
-  { value: 'typescript', text: 'TypeScript', component: javascript },
   { value: 'rust', text: 'Rust', component: rust },
+  { value: 'sql', text: 'SQL', component: sql },
+  { value: 'typescript', text: 'TypeScript', component: javascript },
   { value: 'xml', text: 'XML', component: xml },
   { value: 'yaml', text: 'YAML', component: yaml },
 ];
 
 export {
-  basicSetup,
   EditorState,
   Compartment,
   EditorView,
   keymap,
+  history,
+  defaultKeymap,
+  historyKeymap,
   indentWithTab,
+  HighlightStyle,
+  syntaxHighlighting,
+  tags,
   langItems,
 };
