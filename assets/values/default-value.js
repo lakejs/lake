05be48e8eb9d87302ea96d0c886008f9fa13e2d9
luolonghao <luolonghao@gmail.com>
2024-05-20 17:04:42 +0800

    chore(example): update default value

--- assets/values/default-value.js
+++ assets/values/default-value.js
@@ -122,7 +122,9 @@ window.defaultValue = `
 <lake-box type="block" name="hr"></lake-box>
 <p><lake-box type="inline" name="video" value="eyJ1cmwiOiJodHRwczovL3d3dy55b3V0dWJlLmNvbS93YXRjaD92PTVzTUJoRHY0c2lrIn0="></lake-box></p>
 <p>Think Different</p>
-<p>Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.</p>
+<p>Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo.</p>
+<p>You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius.</p>
+<p>Because the people who are crazy enough to think they can change the world, are the ones who do.</p>
 <p>- Apple Inc.</p>
 <p><br /></p>
 
