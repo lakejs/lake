e82cd382d10a9689b12b828cbc46ad0861b1c841
luolonghao <luolonghao@gmail.com>
2024-07-06 13:35:07 +0800

    test: add cases for keystroke

--- tests/managers/keystroke.test.ts
+++ tests/managers/keystroke.test.ts
@@ -18,7 +18,7 @@ describe('managers / keystroke', () => {
     container.remove();
   });
 
-  it('keydown: should trigger mod+b', () => {
+  it('setKeydown method: should trigger mod+b', () => {
     const keystroke = new Keystroke(container);
     keystroke.setKeydown('mod+b', () => container.html('mod+b'));
     container.emit('keydown', new KeyboardEvent('keydown', {
@@ -29,7 +29,7 @@ describe('managers / keystroke', () => {
     expect(container.html()).to.equal('mod+b');
   });
 
-  it('keydown: should trigger mod+shift+x', () => {
+  it('setKeydown method: should trigger mod+shift+x', () => {
     const keystroke = new Keystroke(container);
     keystroke.setKeydown('mod+shift+x', () => container.html('mod+shift+x'));
     container.emit('keydown', new KeyboardEvent('keydown', {
@@ -41,7 +41,7 @@ describe('managers / keystroke', () => {
     expect(container.html()).to.equal('mod+shift+x');
   });
 
-  it('keydown: should trigger arrow-left', () => {
+  it('setKeydown method: should trigger arrow-left', () => {
     const keystroke = new Keystroke(container);
     keystroke.setKeydown('arrow-left', () => container.html('arrow-left'));
     container.emit('keydown', new KeyboardEvent('keydown', {
@@ -50,7 +50,7 @@ describe('managers / keystroke', () => {
     expect(container.html()).to.equal('arrow-left');
   });
 
-  it('keydown: should trigger mod+]', () => {
+  it('setKeydown method: should trigger mod+]', () => {
     const keystroke = new Keystroke(container);
     keystroke.setKeydown('mod+]', () => container.html('mod+]'));
     container.emit('keydown', new KeyboardEvent('keydown', {
@@ -61,7 +61,7 @@ describe('managers / keystroke', () => {
     expect(container.html()).to.equal('mod+]');
   });
 
-  it('keydown: should trigger enter', () => {
+  it('setKeydown method: should trigger enter', () => {
     const keystroke = new Keystroke(container);
     keystroke.setKeydown('enter', () => container.html('enter'));
     container.emit('keydown', new KeyboardEvent('keydown', {
@@ -71,7 +71,7 @@ describe('managers / keystroke', () => {
     expect(container.html()).to.equal('enter');
   });
 
-  it('keydown: should not trigger enter in composition mode', () => {
+  it('setKeydown method: should not trigger enter in composition mode', () => {
     const keystroke = new Keystroke(container);
     keystroke.setKeydown('enter', () => container.html('enter'));
     container.emit('keydown', new KeyboardEvent('keydown', {
@@ -81,13 +81,27 @@ describe('managers / keystroke', () => {
     expect(container.html()).to.equal('<p>foo</p>');
   });
 
-  it('keyup: should trigger enter', () => {
+  it('setKeyup method: should trigger enter', () => {
     const keystroke = new Keystroke(container);
     keystroke.setKeyup('enter', () => container.html('enter'));
     container.emit('keyup', new KeyboardEvent('keyup', {
       key: 'Enter',
       isComposing: false,
     }));
+    expect(container.html()).to.equal('enter');
+  });
+
+  it('keydown method: should trigger enter', () => {
+    const keystroke = new Keystroke(container);
+    keystroke.setKeydown('enter', () => container.html('enter'));
+    keystroke.keydown('enter');
+    expect(container.html()).to.equal('enter');
+  });
+
+  it('keyup method: should trigger enter', () => {
+    const keystroke = new Keystroke(container);
+    keystroke.setKeyup('enter', () => container.html('enter'));
+    keystroke.keyup('enter');
     expect(container.html()).to.equal('enter');
   });
 
