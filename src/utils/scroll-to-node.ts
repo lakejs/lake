8064b613771ae833200f106a4c9ab3dad03a9a49
luolonghao <luolonghao@gmail.com>
2024-07-26 11:21:09 +0800

    chore(scrollToNode): update comment

--- src/utils/scroll-to-node.ts
+++ src/utils/scroll-to-node.ts
@@ -1,9 +1,8 @@
 import { nodePosition } from './node-position';
 import { Nodes } from '../models/nodes';
 
-// Scrolls the node into the visible area of the browser window
-// if it's not already within the visible area of the browser window.
-// If the node is already within the visibposition.rightle area of the browser window, then no scrolling takes place.
+// If a node is not visible, scrolls the container that contains this node to its position to make it visible.
+// If the node is visible, then scrolling takes place.
 export function scrollToNode(node: Nodes, options?: ScrollIntoViewOptions): void {
   const position = nodePosition(node);
   if (position.left < 0 || position.right < 0 || position.top < 0 || position.bottom < 0) {
