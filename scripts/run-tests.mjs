46a9660318d6f0602e565123d6d326d3d5be54c3
luolonghao <luolonghao@gmail.com>
2024-09-05 10:01:50 +0800

    chore: add comment for puppeteer

--- scripts/run-tests.mjs
+++ scripts/run-tests.mjs
@@ -30,6 +30,11 @@ const step = (msg) => console.log(pc.cyan(msg));
   });
   // Launche a browser and run test cases
   step('Launching a browser instance');
+  // Before v22, Puppeteer launched the old Headless mode by default.
+  // The old headless mode is now known as chrome-headless-shell and ships as a separate binary.
+  // chrome-headless-shell does not match the behavior of the regular Chrome completely
+  // but it is currently more performant for automation tasks where the complete Chrome feature set is not needed.
+  // If the performance is more important for your use case, switch to chrome-headless-shell as following:
   const browser = await puppeteer.launch({
     headless: 'shell',
   });
