5316eadc426049b6333ee366059c928f5456116a
luolonghao <luolonghao@gmail.com>
2025-01-26 21:58:10 +0800

    chore(plugin): improve comments

--- src/managers/plugin.ts
+++ src/managers/plugin.ts
@@ -1,13 +1,16 @@
 import type { Editor } from '../editor';
 import { InitializePlugin, UnmountPlugin } from '../types/plugin';
 
+// The Plugin interface is used to manage a collection of plugins.
 export class Plugin {
-  private pluginMap: Map<string, InitializePlugin> = new Map();
+  private readonly pluginMap: Map<string, InitializePlugin> = new Map();
 
+  // Registers a plugin using a name as the key.
   public add(name: string, plugin: InitializePlugin): void {
     this.pluginMap.set(name, plugin);
   }
 
+  // Loads all registered plugins.
   public loadAll(editor: Editor): Map<string, UnmountPlugin> {
     const unmountPluginMap: Map<string, UnmountPlugin> = new Map();
     for (const name of this.pluginMap.keys()) {
